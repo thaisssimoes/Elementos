470f418e7d968b6309eebc78fcab6575929325e4
Thais <thais@localhost.localdomain>
2016-10-25 17:20:48 -0200

    Organizando melhor

--- Hm.py
+++ Hm.py
@@ -7,9 +7,12 @@ pygame.init()
 
 screen = pygame.display.set_mode((1640,1480))
 elementos = {"h": (255,0,0), "o": (0,255,0), "a": (0,0,255), "pb": (122,122,155)}
-y=50
-
 teclas={"a": pygame.K_a}
+
+y=50
+x=50
+
+
 
 while (True):
     for event in pygame.event.get():
@@ -20,6 +23,7 @@ while (True):
         elif event.type == pygame.KEYDOWN:
             for tecla in teclas:
                 if event.key == teclas[tecla]:
-                    pygame.draw.circle(screen, elementos[tecla], (50,y), 20)
+                    pygame.draw.circle(screen, elementos[tecla], (x,y), 20)
                     pygame.display.update()
+
 
