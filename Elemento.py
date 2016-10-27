5157a119cddc1dfa0462d75d631462429c1c2476
Thaïs Simões <thay.ssim@gmail.com>
2016-10-27 20:47:07 -0200

    Botão direito apaga conjunto de dados

--- Elemento.py
+++ Elemento.py
@@ -3,11 +3,14 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((1240,1080))
-elementos = {frozenset("h"): (255,0,0), frozenset("o"): (0,255,0), frozenset("a"): (0,0,255), frozenset(["p","b"]): (122,122,155)}
+screen = pygame.display.set_mode((1240, 1080))
+elementos = {frozenset("h"): (255, 0, 0), frozenset("o"): (0, 255, 0), frozenset("a"): (0, 0, 255),
+             frozenset(["p", "b"]): (122, 122, 155)}
 pygame.font = pygame.font.SysFont('Arial', 25)
 
 keys = set()
+LEFT=1
+RIGHT=3
 
 def desenhar_circulo():
     try:
@@ -16,10 +19,11 @@ def desenhar_circulo():
     except KeyError:
         cor = elementos[frozenset(["a"])]
 
-    x,y = pygame.mouse.get_pos()
+    x, y = pygame.mouse.get_pos()
     pygame.draw.circle(screen, cor, pygame.mouse.get_pos(), 20)
     screen.blit(pygame.font.render(str(key), True, (255, 255, 255)), (x - 6, y - 14))
     pygame.display.update()
+
 
 def devo_pintar():
     return pressionando and keys
@@ -36,14 +40,12 @@ while True:
             tecla = pygame.key.name(event.key)
             keys.add(tecla)
 
-        elif event.type == pygame.KEYUP:
-            tecla = pygame.key.name(event.key)
-            if tecla == "escape":
-                keys.clear()
-
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pressionando = True
-            desenhar_circulo()
+            if event.button == LEFT:
+                desenhar_circulo()
+            elif event.button == RIGHT:
+                keys.clear()
 
         elif event.type == pygame.MOUSEBUTTONUP:
             pressionando = False
