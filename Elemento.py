bcf529561d2806431213e9544cd081e73e8d60ef
Thais <thais@localhost.localdomain>
2016-10-27 16:45:56 -0200

    Atualizdas funcoes de mouse, teclas e leitura de elementos

--- Elemento.py
+++ Elemento.py
@@ -4,26 +4,46 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((1240,1080))
-elementos = {"h": (255,0,0), "o": (0,255,0), "a": (0,0,255), "pb": (122,122,155)}
-teclas={"a": pygame.K_a, "o": pygame.K_o, "h": pygame.K_h}
+elementos = {frozenset("h"): (255,0,0), frozenset("o"): (0,255,0), frozenset("a"): (0,0,255), frozenset(["p","b"]): (122,122,155)}
 pygame.font = pygame.font.SysFont('Arial', 25)
 
+keys = set()
 
-while (True):
+def desenhar_circulo():
+    try:
+        key = frozenset(keys)
+        cor = elementos[key]
+    except KeyError:
+        cor = elementos[frozenset(["a"])]
+
     x,y = pygame.mouse.get_pos()
+    pygame.draw.circle(screen, cor, pygame.mouse.get_pos(), 20)
+    screen.blit(pygame.font.render(str(key), True, (255, 255, 255)), (x - 6, y - 14))
+    pygame.display.update()
+
+def devo_pintar():
+    return pressionando and keys
+
+
+while True:
     for event in pygame.event.get():
 
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-            elif event.type == pygame.KEYDOWN:
-                for tecla in teclas:
-                    if event.key == teclas[tecla]:
-                        pygame.draw.circle(screen, elementos[tecla], pygame.mouse.get_pos(), 20)
-                        screen.blit(pygame.font.render(tecla, True, (255, 255, 255)), (x-5, y-16) )
-                        pygame.display.update()
+        elif event.type == pygame.KEYDOWN:
+            tecla = pygame.key.name(event.key)
+            keys.add(tecla)
 
+        elif event.type == pygame.KEYUP:
+            tecla = pygame.key.name(event.key)
+            if tecla == "escape":
+                keys.clear()
 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            pressionando = True
+            desenhar_circulo()
 
-
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pressionando = False
