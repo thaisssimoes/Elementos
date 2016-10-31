4ba78c182bcf4f589744748a83a86f600f8bbb31
Thais <thais@localhost.localdomain>
2016-10-31 15:50:47 -0200

    Consertada a centralizacao da sigla

--- Elemento.py
+++ Elemento.py
@@ -3,7 +3,7 @@ import sys
 
 
 
-class Elem(frozenset):
+class Elem(tuple):
     def __str__(self):
         return ''.join(e for e in self)
 
@@ -12,12 +12,13 @@ pygame.init()
 
 screen = pygame.display.set_mode((1240, 1080))
 elementos = {Elem("h"): (255, 0, 0), Elem("o"): (0, 255, 0), Elem("a"): (0, 0, 255),
-             Elem(["p", "b"]): (122, 122, 155)}
+             Elem(["p", "b"]): (122, 122, 155), Elem(["u", "u", "s"]): (125, 210, 130)}
 pygame.font = pygame.font.SysFont('Arial', 25)
 
-keys = set()
+keys = []
 LEFT=1
 RIGHT=3
+RADIUS = 30
 
 def desenhar_circulo():
     key = Elem(keys)
@@ -31,8 +32,10 @@ def desenhar_circulo():
 
     siglas=pygame.font.render(str(key), True, (255, 255, 255))
 
-    pygame.draw.circle(screen, cor, pygame.mouse.get_pos(), 20)
-    screen.blit(siglas, (x-13, y-15))
+    sigla = siglas.get_rect()
+
+    pygame.draw.circle(screen, cor, (x,y), RADIUS)
+    screen.blit(siglas, (x-sigla.centerx, y-sigla.centery))
     pygame.display.update()
 
 
@@ -49,14 +52,13 @@ while True:
 
         elif event.type == pygame.KEYDOWN:
             tecla = pygame.key.name(event.key)
-            keys.add(tecla)
+            keys.append(tecla)
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pressionando = True
             if event.button == LEFT:
                 desenhar_circulo()
             elif event.button == RIGHT:
-                keys.clear()
-
+                keys = []
         elif event.type == pygame.MOUSEBUTTONUP:
             pressionando = False
