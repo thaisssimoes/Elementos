892dd152df88cdfc85d04d18a97d6cb375e28db1
Thaïs Simões <thay.ssim@gmail.com>
2016-11-01 14:23:10 -0200

    Orientado a objetos

--- Elemento.py
+++ Elemento.py
@@ -1,64 +1,40 @@
 import pygame
-import sys
-
-
 
 class Elem(tuple):
     def __str__(self):
         return ''.join(e for e in self)
 
 
-pygame.init()
+class elemento():
+    def __init__(self):
+        pygame.init()
 
-screen = pygame.display.set_mode((1240, 1080))
-elementos = {Elem("h"): (255, 0, 0), Elem("o"): (0, 255, 0), Elem("a"): (0, 0, 255),
-             Elem(["p", "b"]): (122, 122, 155), Elem(["u", "u", "s"]): (125, 210, 130)}
-pygame.font = pygame.font.SysFont('Arial', 25)
+        self.screen = pygame.display.set_mode((1240, 1080))
+        self.elementos = {Elem("h"): (255, 0, 0),
+                          Elem("o"): (0, 255, 0),
+                          Elem("a"): (0, 0, 255),
+                          Elem(["p", "b"]): (122, 122, 155),
+                          Elem(["u", "u", "s"]): (125, 210, 130)}
 
-keys = []
-LEFT=1
-RIGHT=3
-RADIUS = 30
+        pygame.font = pygame.font.SysFont('Arial', 25)
+        self.keys = []
 
-def desenhar_circulo():
-    key = Elem(keys)
-    try:
-        cor = elementos[key]
-    except KeyError:
-        key = Elem("a")
-        cor = elementos[key]
+        self.RADIUS = 30
 
-    x, y = pygame.mouse.get_pos()
+    def desenhar_circulo(self):
+        self.key = Elem(self.keys)
+        try:
+            cor = self.elementos[self.key]
+        except KeyError:
+            self.key = Elem("a")
+            cor = self.elementos[self.key]
 
-    siglas=pygame.font.render(str(key), True, (255, 255, 255))
+        self.x, self.y = pygame.mouse.get_pos()
+        pygame.draw.circle(self.screen, cor, (self.x,self.y), self.RADIUS)
+        self.escrever_elemento()
+        pygame.display.update()
 
-    sigla = siglas.get_rect()
-
-    pygame.draw.circle(screen, cor, (x,y), RADIUS)
-    screen.blit(siglas, (x-sigla.centerx, y-sigla.centery))
-    pygame.display.update()
-
-
-def devo_pintar():
-    return pressionando and keys
-
-
-while True:
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        elif event.type == pygame.KEYDOWN:
-            tecla = pygame.key.name(event.key)
-            keys.append(tecla)
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            pressionando = True
-            if event.button == LEFT:
-                desenhar_circulo()
-            elif event.button == RIGHT:
-                keys = []
-        elif event.type == pygame.MOUSEBUTTONUP:
-            pressionando = False
+    def escrever_elemento(self):
+        self.siglas = pygame.font.render(str(self.key), True, (255, 255, 255))
+        self.sigla = self.siglas.get_rect()
+        self.screen.blit(self.siglas, (self.x-self.sigla.centerx, self.y-self.sigla.centery))
